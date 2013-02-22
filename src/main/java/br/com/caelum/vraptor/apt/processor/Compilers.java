07f17fd667b844939036b624029e6b857afa8d0a
Mario Amaral <mario.fts@gmail.com>
2013-02-22 17:21:55 -0300

    Removendo classes do eclipe (por enquanto)

--- src/main/java/br/com/caelum/vraptor/apt/processor/Compilers.java
+++ src/main/java/br/com/caelum/vraptor/apt/processor/Compilers.java
@@ -1,10 +1,8 @@
 package br.com.caelum.vraptor.apt.processor;
 
 import javax.annotation.processing.ProcessingEnvironment;
-import org.eclipse.jdt.internal.apt.pluggable.core.dispatch.IdeBuildProcessingEnvImpl;
 
 import br.com.caelum.vraptor.apt.TheCompiler;
-import br.com.caelum.vraptor.apt.eclipse.EclipseCompiler;
 import br.com.caelum.vraptor.apt.javac.JavacCompiler;
 
 import com.sun.tools.javac.processing.JavacProcessingEnvironment;
@@ -14,8 +12,6 @@ public class Compilers {
 	public static TheCompiler getCompilerFor(ProcessingEnvironment processingEnv) {
 		if (processingEnv instanceof JavacProcessingEnvironment){
 			return new JavacCompiler(processingEnv);
-		}else if(processingEnv instanceof IdeBuildProcessingEnvImpl){
-			return new EclipseCompiler(processingEnv);
 		}
 		return null;
 		
